4b954d20dcebf3258094ce18a601d3ead24addd0
Joanna Grochal <joanna.k.grochal@gmail.com>
2019-11-22 16:51:04 +0100

    Zestaw 2 zadanie 2

--- 2_2/zad_2.py
+++ 2_2/zad_2.py
@@ -1,22 +1,27 @@
 import numpy as np
 
 
-passwd = 'abc'
-plaintxt = 'testtest'
-
-
 class Vigenere:
     ALPHABET_LENGTH = 26
 
-    def __init__(self, key, plaintext):
+    def __init__(self, input_str, key, mode):
         self.key = key
-        self.numeric_key = self.str_to_num(self.key)
-        self.plaintext = plaintext.lower()
-        self.numeric_plaintext = self.str_to_num(self.plaintext)
-        self.numeric_key_ext = np.resize(self.numeric_key, len(self.plaintext))
+        self.mode = mode
+        self.plaintext = ''
+        self.numeric_plaintext = []
         self.ciphertext = ''
         self.numeric_ciphertext = []
-        self.encryption()
+
+        self.init_encryption(input_str) if self.mode == 'e' else self.init_decryption(input_str)
+
+        self.numeric_key = self.str_to_num(self.key)
+        self.numeric_key_ext = np.resize(self.numeric_key, len(self.plaintext + self.ciphertext))
+
+        self.encryption() if self.mode == 'e' else self.decryption()
+
+    def init_encryption(self, input_str):
+        self.plaintext = input_str.lower()
+        self.numeric_plaintext = self.str_to_num(self.plaintext)
 
     def encryption(self):
         self.numeric_ciphertext = self.numeric_plaintext + self.numeric_key_ext
@@ -25,8 +30,16 @@ class Vigenere:
         self.ciphertext = self.num_to_str(self.numeric_ciphertext)
         print(self.ciphertext)
 
+    def init_decryption(self, input_str):
+        self.ciphertext = input_str.lower()
+        self.numeric_ciphertext = self.str_to_num(self.ciphertext)
+
     def decryption(self):
-        pass
+        self.numeric_plaintext = self.numeric_ciphertext - self.numeric_key_ext
+        self.numeric_plaintext = [item % self.ALPHABET_LENGTH for item in self.numeric_plaintext]
+        print(self.ciphertext)
+        self.plaintext = self.num_to_str(self.numeric_plaintext)
+        print(self.plaintext)
 
 
     @staticmethod
@@ -39,4 +52,8 @@ class Vigenere:
 
 
 if __name__ == '__main__':
-    Vigenere(passwd, plaintxt)
+    key = 'abc'
+    input_str = 'tfutugsu'
+    # mode = 'e'
+    mode = 'd'
+    Vigenere(input_str, key, mode)
